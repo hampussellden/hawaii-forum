c76b3b854a0971508e4e22e3f6532285d8ea14d2
hampussellden <hampussellden1@gmail.com>
2023-02-27 14:09:28 +0100

    db connection and migrations
    - created models
    - migrations for DB

---
+++ app/Models/Thread.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Thread extends Model
+{
+    use HasFactory;
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
