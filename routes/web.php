c233846b01e1215a9ebfe1612dba358e2f05d033
hampussellden <hampussellden1@gmail.com>
2023-03-03 13:57:52 +0100

    removed reduntant controller

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\PostsController;
-use App\Http\Controllers\ThreadsPageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterUserController;
 use App\Http\Controllers\ThreadController;
